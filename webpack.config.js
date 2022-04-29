fd0477af2fd459a0b5d95c718963df522843a059
EmmanouilM <manosgr55@hotmail.com>
2022-04-29 13:15:38 +0200

    add webpack clean and html plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,30 @@
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-let mode = "development"
-let target = "web"
-if  (process.env.NODE_ENV === "production") {
-    mode = "production";
-    target = "browserslist";
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+let mode = "development";
+let target = "web";
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+  target = "browserslist";
 }
 
 module.exports = {
   mode: mode,
   target: target,
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
   module: {
     rules: [
       {
         test: /\.(s[ac]|c)ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader","sass-loader" ],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.jsx?$/,
@@ -24,13 +35,19 @@ module.exports = {
       },
     ],
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
   resolve: {
-    extensions:[".js", ".jsx"]
+    extensions: [".js", ".jsx"],
   },
   devtool: "source-map",
   devServer: {
     static: "./dist",
-    hot:true
+    hot: true,
   },
 };
